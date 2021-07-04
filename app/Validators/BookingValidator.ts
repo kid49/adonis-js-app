007e6414222c05b12c210bc75dd4a331c52f9a7a
bayupb <kampuspb.bayu@gmail.com>
2021-07-04 16:39:29 +0700

    master

--- app/Validators/BookingValidator.ts
+++ app/Validators/BookingValidator.ts
@@ -1,5 +1,5 @@
-import { schema,rules } from '@ioc:Adonis/Core/Validator'
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from "@ioc:Adonis/Core/Validator";
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 export default class BookingValidator {
   constructor(protected ctx: HttpContextContract) {}
